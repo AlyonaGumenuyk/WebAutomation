d67c9f1e1ae66f6771363d6affd8cd0ed297e2d7
KostyaLukyanchikov <mrwithoutnickname@gmail.com>
2020-09-23 16:27:16 +0300

    fix

--- client/worker.py
+++ client/worker.py
@@ -18,7 +18,8 @@ class Worker:
         else:
             options = Options()
             options.headless = True
-            self.driver = webdriver.Firefox(log_path='logs/geckodriver.log', options=options)
+            self.driver = webdriver.Firefox(executable_path='drivers/firefoxdriver/geckodriver.exe',
+                                            log_path='logs/geckodriver.log', options=options)
             self.server_address = 'http://0.0.0.0:8080/'
         self.driver.set_window_size(1920, 1080)
         self.time_to_sleep = 10
