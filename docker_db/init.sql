181c44a4bd5492b7fd9c6bbaf4a7a174936feb6f
KostyaLukyanchikov <mrwithoutnickname@gmail.com>
2020-10-02 19:40:53 +0300

    fix

--- docker_db/init.sql
+++ docker_db/init.sql
@@ -1,6 +1,5 @@
 CREATE DATABASE "1xStavkaDB";
 \c "1xStavkaDB"
---CREATE USER docker WITH PASSWORD 'docker';
 GRANT ALL PRIVILEGES ON DATABASE "1xStavkaDB" to docker;
 CREATE TABLE tasks (
     id SERIAL, skill varchar(20),
@@ -9,5 +8,16 @@ CREATE TABLE tasks (
     worker_type varchar(20),
     state varchar(30)
 );
---CREATE TABLE results (id SERIAL, task_id int, skill varchar(20), result json, executed_state varchar(20));
---CREATE TABLE games (id SERIAL, datetime timestamp, tournament text, left_command text, right_command text);
+CREATE TABLE results (
+    id SERIAL, task_id int,
+    skill varchar(20),
+    result json,
+    executed_state varchar(20)
+);
+CREATE TABLE games (
+    id SERIAL,
+    datetime timestamp,
+    tournament text,
+    left_command text,
+    right_command text
+);
