dc1e1ef734a83e4a712c410bdf9055141f6cd767
KostyaLukyanchikov <mrwithoutnickname@gmail.com>
2020-10-02 18:49:20 +0300

    fix

--- docker_db/init.sql
+++ docker_db/init.sql
@@ -1,9 +1,3 @@
 CREATE TABLE tasks (id SERIAL, skill varchar(20), arguments json, attempts smallint, worker_type varchar(20), state varchar(30));
 CREATE TABLE results (id SERIAL, task_id int, skill varchar(20), result json, executed_state varchar(20));
 CREATE TABLE games (id SERIAL, datetime timestamp, tournament text, left_command text, right_command text);
-
-
-psql --command "\c 1xStavkaDB" &&\
-    psql --command "CREATE TABLE tasks (id SERIAL, skill varchar(20), arguments json, attempts smallint, worker_type varchar(20), state varchar(30));" &&\
-    psql --command "CREATE TABLE results (id SERIAL, task_id int, skill varchar(20), result json, executed_state varchar(20));" &&\
-    psql --command "CREATE TABLE games (id SERIAL, datetime timestamp, tournament text, left_command text, right_command text);"
