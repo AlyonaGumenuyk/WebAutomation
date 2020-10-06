b47bbad70a5ab4f836fbb2bf83b2ba6ee0b4903c
KostyaLukyanchikov <mrwithoutnickname@gmail.com>
2020-10-06 13:57:31 +0300

    fix

--- db_helpers/db_helper.py
+++ db_helpers/db_helper.py
@@ -7,11 +7,8 @@ class DBHelper:
 
     def __init__(self):
         self.user = 'docker'
-        self.host = '172.18.0.1'
+        self.host = '172.17.0.1'
         self.password = 'docker'
-        # self.user = 'postgres'
-        # self.host = 'localhost'
-        # self.password = 'boss1234'
         self.conn = None
         self.cur = None
         self.stavka_db = '1xStavkaDB'
